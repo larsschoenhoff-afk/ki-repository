d0a122874ecc1c953f6c70bcc8483e2907d906d6
larsschoenhoff-afk <lars.schoenhoff@googlemail.com>
2026-03-06 17:18:05 +0100

    Register MyFaces startup listener for factory initialization

--- src/main/java/com/example/ki_demo/config/JsfConfig.java
+++ src/main/java/com/example/ki_demo/config/JsfConfig.java
@@ -1,12 +1,19 @@
 package com.example.ki_demo.config;
 
 import jakarta.faces.webapp.FacesServlet;
+import org.apache.myfaces.webapp.StartupServletContextListener;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JsfConfig {
+
+    @Bean
+    public ServletListenerRegistrationBean<StartupServletContextListener> myFacesStartupListener() {
+        return new ServletListenerRegistrationBean<>(new StartupServletContextListener());
+    }
 
     @Bean
     public ServletRegistrationBean<FacesServlet> facesServlet() {
