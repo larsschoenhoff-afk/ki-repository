d73a4d39b1ade6774ea21f252c29eb892f171c1c
larsschoenhoff-afk <lars.schoenhoff@googlemail.com>
2026-03-06 16:54:37 +0100

    Initialize Mojarra factories via ConfigureListener

--- src/main/java/com/example/ki_demo/config/JsfConfig.java
+++ src/main/java/com/example/ki_demo/config/JsfConfig.java
@@ -1,6 +1,8 @@
 package com.example.ki_demo.config;
 
+import com.sun.faces.config.ConfigureListener;
 import jakarta.faces.webapp.FacesServlet;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +15,10 @@ public class JsfConfig {
         ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
         registration.setLoadOnStartup(1);
         return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<>(new ConfigureListener());
     }
 }
