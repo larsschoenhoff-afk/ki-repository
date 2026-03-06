9174c5b8a79058484810040837c285bc46dc02ed
larsschoenhoff-afk <lars.schoenhoff@googlemail.com>
2026-03-06 17:13:58 +0100

    Switch JSF runtime to MyFaces and remove Mojarra listener

--- src/main/java/com/example/ki_demo/config/JsfConfig.java
+++ src/main/java/com/example/ki_demo/config/JsfConfig.java
@@ -1,9 +1,6 @@
 package com.example.ki_demo.config;
 
-import com.sun.faces.config.ConfigureListener;
 import jakarta.faces.webapp.FacesServlet;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,19 +13,5 @@ public class JsfConfig {
         ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
         registration.setLoadOnStartup(1);
         return registration;
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
-        return new ServletListenerRegistrationBean<>(new ConfigureListener());
-    }
-
-    @Bean
-    public ServletContextInitializer jsfServletContextInitializer() {
-        return servletContext -> {
-            servletContext.setInitParameter("com.sun.faces.enableCdiResolverChain", "false");
-            servletContext.setInitParameter("jakarta.faces.ENABLE_CDI_RESOLVER_CHAIN", "false");
-            servletContext.setInitParameter("jakarta.faces.DISABLE_CDI_RESOLVER_CHAIN", "true");
-        };
     }
 }
