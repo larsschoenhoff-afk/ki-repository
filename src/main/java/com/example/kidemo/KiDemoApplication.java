7352dd65fcf4830d5057ad92fc53283a22a7c7ae
larsschoenhoff-afk <lars.schoenhoff@googlemail.com>
2026-03-06 17:41:47 +0100

    Add simple Thymeleaf Hello World webapp

---
+++ src/main/java/com/example/kidemo/KiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.kidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiDemoApplication.class, args);
+    }
+}
