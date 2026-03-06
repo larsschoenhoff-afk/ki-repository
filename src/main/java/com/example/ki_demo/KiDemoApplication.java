d4e2aaf5f8ee084d648f11d6207ba28ed566cbe8
Lars Schoenhoff <Lars.Schoenhoff@wpk.de>
2026-03-06 15:54:18 +0100

    Initial

---
+++ src/main/java/com/example/ki_demo/KiDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.ki_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiDemoApplication.class, args);
+		System.out.println("Meine KI Demo ist gestartet!");
+	}
+
+}
