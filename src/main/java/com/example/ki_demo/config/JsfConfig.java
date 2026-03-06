746c3f89c7d22d5747ee30cc84e0eb35c7c9bd8c
larsschoenhoff-afk <lars.schoenhoff@googlemail.com>
2026-03-06 17:01:36 +0100

    Disable JSF CDI resolver chain for non-CDI runtime

--- src/main/java/com/example/ki_demo/config/JsfConfig.java
+++ src/main/java/com/example/ki_demo/config/JsfConfig.java
@@ -2,6 +2,7 @@ package com.example.ki_demo.config;
 
 import com.sun.faces.config.ConfigureListener;
 import jakarta.faces.webapp.FacesServlet;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +21,14 @@ public class JsfConfig {
     @Bean
     public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
         return new ServletListenerRegistrationBean<>(new ConfigureListener());
+    }
+
+    @Bean
+    public ServletContextInitializer jsfServletContextInitializer() {
+        return servletContext -> {
+            servletContext.setInitParameter("com.sun.faces.enableCdiResolverChain", "false");
+            servletContext.setInitParameter("jakarta.faces.ENABLE_CDI_RESOLVER_CHAIN", "false");
+            servletContext.setInitParameter("jakarta.faces.DISABLE_CDI_RESOLVER_CHAIN", "true");
+        };
     }
 }
