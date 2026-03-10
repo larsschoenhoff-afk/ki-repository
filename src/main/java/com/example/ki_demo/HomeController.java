bd0cc6219e82c4a97b0db485cc3eb4b76265005e
larsschoenhoff-afk <lars.schoenhoff@googlemail.com>
2026-03-10 12:51:01 +0100

    Add 1000 sample rows and configurable table paging

--- src/main/java/com/example/ki_demo/HomeController.java
+++ src/main/java/com/example/ki_demo/HomeController.java
@@ -5,19 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        List<AddressEntry> entries = List.of(
-                new AddressEntry("Max Mustermann", "Hauptstraße 1", "Berlin", "10115", "+49 30 1234567"),
-                new AddressEntry("Erika Musterfrau", "Bahnhofstraße 23", "Hamburg", "20095", "+49 40 2345678"),
-                new AddressEntry("Lena Schmidt", "Rosenweg 5", "München", "80331", "+49 89 3456789"),
-                new AddressEntry("Paul Keller", "Lindenallee 14", "Köln", "50667", "+49 221 4567890"),
-                new AddressEntry("Sarah Neumann", "Marktplatz 9", "Frankfurt am Main", "60311", "+49 69 5678901")
-        );
+        List<AddressEntry> entries = IntStream.rangeClosed(1, 1000)
+                .mapToObj(index -> new AddressEntry(
+                        "Beispielsatz " + index,
+                        "Musterstraße " + index,
+                        "Musterstadt " + ((index % 20) + 1),
+                        String.format("%05d", 10000 + index),
+                        "+49 30 " + String.format("%07d", index)
+                ))
+                .toList();
 
         model.addAttribute("entries", entries);
         return "index";
